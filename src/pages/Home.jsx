803118793db207d184bb3b4fc528e3458bd19778
joaopjpc <joaopedropaixaocorrea@gmail.com>
2024-03-31 11:53:57 -0300

    Segunda tela

---
+++ src/pages/Home.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Header from '../components/Header'
+import Options from '../components/Options'
+import CiclistaMeio from '../components/CiclistaMeio'
+import End from '../components/End'
+import HeaderVenda from '../components/HeaderVenda'
+import MeioVendas from '../components/MeioVendas'
+import PreencherVenda from '../components/PreencherVenda'
+
+/*
+1     <Header></Header>
+      <Options></Options>
+      <CiclistaMeio></CiclistaMeio>
+      <End></End>
+
+2     <HeaderVenda></HeaderVenda>
+      <MeioVendas></MeioVendas>
+      <PreencherVenda></PreencherVenda>
+*/
+
+function Home() {
+  return (
+    <div>
+      
+    
+    </div>
+  )
+}
+
+export default Home
