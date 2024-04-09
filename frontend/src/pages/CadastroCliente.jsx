210d2021ac4b6ad0fa53fb0efb906c2e4b3f158b
RodrigoKasama <rodrigo.parracho@gmail.com>
2024-04-09 12:59:21 -0300

    Centralização do Form e Redução de Controles

--- frontend/src/pages/CadastroCliente.jsx
+++ frontend/src/pages/CadastroCliente.jsx
@@ -2,9 +2,7 @@ import FormCliente from "../components/FomCadastroCliente";
 
 function CadastroCliente() {
 	return (
-		<div>
-			<FormCliente></FormCliente>
-		</div>
+		<FormCliente></FormCliente>
 	)
 }
 
