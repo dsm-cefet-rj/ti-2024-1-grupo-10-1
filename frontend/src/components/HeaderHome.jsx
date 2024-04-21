b6f4ce9181f1d1eebc761c6e1828a67ce3d7221d
RodrigoKasama <rodrigo.parracho@gmail.com>
2024-04-21 10:11:18 -0300

    Seçoes de Conta, Favoritos e Anuncio com restrição e redirecionamento de Login

--- frontend/src/components/HeaderHome.jsx
+++ frontend/src/components/HeaderHome.jsx
@@ -12,8 +12,7 @@ import useUserStore from './UserUtils';
 const HeaderHome = () => {
 	const [menuOpen, setMenuOpen] = useState(false);
 
-	const { user, has_logged } = useUserStore((state) => ({
-		user: state.user,
+	const { has_logged } = useUserStore((state) => ({
 		has_logged: state.user.logged
 	}));
 
@@ -34,21 +33,25 @@ const HeaderHome = () => {
 
 				{/* Menu de Navegação para Dispositivos Maiores */}
 				<nav className="hidden md:flex items-center gap-7">
-					<Link to="/favoritos" className='cursor-pointer flex items-center gap-2'><img src={fav} alt="Loja" /> <span>Favoritos</span></Link>
-					<Link to="/feedback" className='cursor-pointer flex items-center gap-2'><img src={batePapo} alt="Bate Papo" /> <span>Feedbacks</span></Link>
-					{/* <Link to="/conta" className='cursor-pointer flex items-center gap-2'><img src={ciclista} alt="Ciclista" /> <span>Conta</span></Link> */}
 
+					{/* <Link to="/favoritos" className='cursor-pointer flex items-center gap-2'><img src={fav} alt="Loja" /> <span>Favoritos</span></Link> */}
+					{has_logged && <Link to="/favoritos" className='cursor-pointer flex items-center gap-2'><img src={fav} alt="Loja" /> <span>Favoritos</span></Link>}
+					{!has_logged && <Link to="/login" className='cursor-pointer flex items-center gap-2'><img src={fav} alt="Loja" /> <span>Favoritos</span></Link>} {/*Adiciona o componente B */}
+
+					<Link to="/feedback" className='cursor-pointer flex items-center gap-2'><img src={batePapo} alt="Bate Papo" /> <span>Feedbacks</span></Link>
+
+					{/* <Link to="/conta" className='cursor-pointer flex items-center gap-2'><img src={ciclista} alt="Ciclista" /> <span>Conta</span></Link> */}
 					{has_logged && <Link to="/conta" className='cursor-pointer flex items-center gap-2'><img src={ciclista} alt="Ciclista" /> <span>Conta</span></Link>}
 					{!has_logged && <Link to="/login" className='cursor-pointer flex items-center gap-2'><img src={ciclista} alt="Ciclista" /> <span>Conta</span></Link>} {/*Adiciona o componente B */}
 
-					<Link to="/anuncios" className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mx-2">Quero Anunciar</Link>
+					{/* <Link to="/anuncios" className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mx-2">Quero Anunciar</Link> */}
+					{has_logged && <Link to="/anuncios" className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mx-2">Quero Anunciar</Link>}
+					{!has_logged && <Link to="/login" className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mx-2">Quero Anunciar</Link>}
+					
 				</nav>
 
 				{/* Botão de Hambúrguer */}
-				<button
-					className="md:hidden focus:outline-none flex items-center justify-center"
-					onClick={() => setMenuOpen(!menuOpen)}
-				>
+				<button className="md:hidden focus:outline-none flex items-center justify-center" onClick={() => setMenuOpen(!menuOpen)} >
 					<svg className="w-6 h-6 text-gray-900" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 						<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
 					</svg>
@@ -58,20 +61,30 @@ const HeaderHome = () => {
 				{menuOpen && (
 					<div className="md:hidden absolute top-0 left-0 w-full h-screen bg-white flex flex-col items-center justify-center">
 						{/* Botão de Fechar */}
-						<button
-							className="absolute top-4 right-4 focus:outline-none"
-							onClick={() => setMenuOpen(false)}
-						>
+						<button className="absolute top-4 right-4 focus:outline-none" onClick={() => setMenuOpen(false)}>
 							<svg className="w-6 h-6 text-gray-900" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 								<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
 							</svg>
 						</button>
 
 						{/* Itens do Menu de Hambúrguer */}
-						<Link to="/favoritos" className='cursor-pointer flex items-center gap-2 mb-4'><b>Favoritos</b></Link>
+						
+						{/* <Link to="/favoritos" className='cursor-pointer flex items-center gap-2 mb-4'><b>Favoritos</b></Link> */}
+						{has_logged && <Link to="/favoritos" className='cursor-pointer flex items-center gap-2 mb-4'><b>Favoritos</b></Link>}
+						{!has_logged && <Link to="/login" className='cursor-pointer flex items-center gap-2 mb-4'><b>Favoritos</b></Link>}
+
 						<Link to="/feedback" className='cursor-pointer flex items-center gap-2 mb-4'><b>Feedbacks</b></Link>
-						<Link to="/conta" className='cursor-pointer flex items-center gap-2 mb-4'><b>Minha conta</b></Link>
+						{/* {has_logged && <Link to="/feedback" className='cursor-pointer flex items-center gap-2 mb-4'><b>Feedbacks</b></Link>} */}
+						{/* {!has_logged && <Link to="/login" className='cursor-pointer flex items-center gap-2 mb-4'><b>Feedbacks</b></Link>} */}
+
+						{/* <Link to="/conta" className='cursor-pointer flex items-center gap-2 mb-4'><b>Minha conta</b></Link> */}
+						{has_logged && <Link to="/conta" className='cursor-pointer flex items-center gap-2 mb-4'><b>Minha conta</b></Link>}
+						{!has_logged && <Link to="/login" className='cursor-pointer flex items-center gap-2 mb-4'><b>Minha conta</b></Link>}
+						
+
 						<Link to="/quero-vender" className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2">Quero Anunciar</Link>
+						{has_logged && <Link to="/quero-vender" className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2">Quero Anunciar</Link>}
+						{!has_logged && <Link to="/login" className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2">Quero Anunciar</Link>}
 					</div>
 				)}
 			</header>
