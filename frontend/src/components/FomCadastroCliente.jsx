b4c33002cc862c7f6541e73a76039463832b482e
Gabriel-gag <115362448+Gabriel-gag@users.noreply.github.com>
2024-04-21 01:39:02 -0300

    Update FomCadastroCliente.jsx
    correções

--- frontend/src/components/FomCadastroCliente.jsx
+++ frontend/src/components/FomCadastroCliente.jsx
@@ -112,7 +112,7 @@ export default function FormCliente() {
 									{/* Div de input Numero */}
 									<div className="sm:col-span-1">
 										<label htmlFor="city" className="block text-sm font-medium leading-6 text-gray-900">
-											Numero
+											Número
 										</label>
 										<div className="mt-2">
 											<input
@@ -136,7 +136,7 @@ export default function FormCliente() {
 									<span className="inline-block mr-2">Cancel</span>
 								</Link>
 							</button>
-							<button onClick={handleSubmit} type="button" className="rounded-md bg-purple-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+							<button onClick={handleSubmit} type="submit" className="rounded-md bg-purple-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
 								<Link to="/home">
 									<span className="inline-block mr-2">Save</span>
 								</Link>
