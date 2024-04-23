d958ac973783db34caabfc13b69f923373577539
RodrigoKasama <rodrigo.parracho@gmail.com>
2024-04-23 18:54:09 -0300

    Ajuste dos itens recomendados

--- frontend/src/components/Recomendados.jsx
+++ frontend/src/components/Recomendados.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { fetchProducts } from './BackendUtils';
+import { fetchAllProducts } from './BackendUtils';
 import useUserStore from './UserUtils';
 
 
@@ -9,15 +9,14 @@ const Recomendados = () => {
 	const { has_logged } = useUserStore((state) => ({ has_logged: state.user.logged }));
 
 	useEffect(() => {
-		fetchProducts(setProdutos);
+		fetchAllProducts(setProdutos);
 	}, []);
 
 	console.log(produtos);
 	return (
 		<div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-4 mt-10 mx-5">
 			{produtos.map((produto) => (
-				console.log(produto.imagem),
-				<Link key={produto.id} to={(has_logged) ? `/bike/${produto.id_bike}` : "/login"} className="relative bg-black">
+				<Link key={produto.id_bike} to={(has_logged) ? `/bike/${produto.id_bike}` : "/login"} className="relative bg-black">
 					<img src={produto.imagem} className="object-cover w-full h-72" alt={produto.titulo} />
 					<div className="absolute inset-x-0 bottom-0 bg-black bg-opacity-75 text-white px-4 py-2">
 						<p className="text-lg">Preço: {produto.valor}</p>
