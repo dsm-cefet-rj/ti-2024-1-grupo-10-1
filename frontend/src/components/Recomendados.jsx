033339961496a4626dc540523b2aa60d200ada97
joaopjpc <joaopedropaixaocorrea@gmail.com>
2024-04-23 17:30:19 -0300

    bike nova no banco

--- frontend/src/components/Recomendados.jsx
+++ frontend/src/components/Recomendados.jsx
@@ -6,37 +6,37 @@ import useUserStore from './UserUtils';
 
 
 const Recomendados = () => {
-  const [produtos, setProdutos] = useState([]);
-  const { has_logged } = useUserStore((state) => ({ has_logged: state.user.logged }))
-  useEffect(() => {
-   // const getProducts = async () => {
-    //  try {
-      //  const response = await fetchProducts(setProdutos);
-       // console.log('Produtos:', response);
-      //} catch (error) {
-     //   console.error('Erro ao buscar produtos:', error);
-     // }
-    //};
+	const [produtos, setProdutos] = useState([]);
+	const { has_logged } = useUserStore((state) => ({ has_logged: state.user.logged }))
+	useEffect(() => {
+	 // const getProducts = async () => {
+		//  try {
+			//  const response = await fetchProducts(setProdutos);
+			 // console.log('Produtos:', response);
+			//} catch (error) {
+		 //   console.error('Erro ao buscar produtos:', error);
+		 // }
+		//};
 
-    fetchProducts(setProdutos);
+		fetchProducts(setProdutos);
 	
-  }, []);
+	}, []);
 
 console.log(produtos)
-  return (
-    <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-4 mt-10 mx-5">
-      {produtos.map((produto) => (
-        console.log(produto.imagem),
-        <Link key={produto.id} to={ (has_logged)?`/bike/${produto.id_bike}`: "/login"} className="relative bg-black">
-          <img src={produto.imagem} className="object-cover w-full h-72" alt={produto.titulo} />
-          <div className="absolute inset-x-0 bottom-0 bg-black bg-opacity-75 text-white px-4 py-2">
-            <p className="text-lg">Preço: {produto.valor}</p>
-            <p className="text-sm">{produto.descricao}</p>
-          </div>
-        </Link>
-      ))}
-    </div>
-  );
+	return (
+		<div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-4 mt-10 mx-5">
+			{produtos.map((produto) => (
+				console.log(produto.imagem),
+				<Link key={produto.id} to={ (has_logged)?`/bike/${produto.id_bike}`: "/login"} className="relative bg-black">
+					<img src={produto.imagem} className="object-cover w-full h-72" alt={produto.titulo} />
+					<div className="absolute inset-x-0 bottom-0 bg-black bg-opacity-75 text-white px-4 py-2">
+						<p className="text-lg">Preço: {produto.valor}</p>
+						<p className="text-sm">{produto.descricao}</p>
+					</div>
+				</Link>
+			))}
+		</div>
+	);
 };
 
 export default Recomendados;
