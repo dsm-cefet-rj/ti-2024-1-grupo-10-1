a01f8646ba277f1443ddcc8c4ab7bdf26e7b591c
joaopjpc <joaopedropaixaocorrea@gmail.com>
2024-04-23 17:06:02 -0300

    banco atualizado

--- frontend/src/components/Recomendados.jsx
+++ frontend/src/components/Recomendados.jsx
@@ -31,7 +31,7 @@ console.log(produtos)
           <img src={produto.imagem} className="object-cover w-full h-72" alt={produto.titulo} />
           <div className="absolute inset-x-0 bottom-0 bg-black bg-opacity-75 text-white px-4 py-2">
             <p className="text-lg">Preço: {produto.valor}</p>
-            <p className="text-sm">{produto.Descricao}</p>
+            <p className="text-sm">{produto.descricao}</p>
           </div>
         </Link>
       ))}
