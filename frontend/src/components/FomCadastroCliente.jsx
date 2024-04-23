0382c118dbf5fe1ee64c2467077f5f4b54e6ff61
Gabriel-gag <115362448+Gabriel-gag@users.noreply.github.com>
2024-04-23 17:07:38 -0300

    Funcionalidade nova tela

--- frontend/src/components/FomCadastroCliente.jsx
+++ frontend/src/components/FomCadastroCliente.jsx
@@ -60,7 +60,7 @@ export default function FormCliente() {
 
 								
 
-								{/* Div de input CEP */}
+								{/* Div de input Telefone */}
 								<div className="mt-2">
 									<label htmlFor="postal-code" className="block text-sm font-medium leading-6 text-gray-900">Telefone</label>
 									<input type="text" pattern='[0-9]{8}' name="postal-code" id="postal-code" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" required />
---
+++ frontend/src/pages/EditarCadastro.jsx
@@ -0,0 +1,14 @@
+import EditarCadastro from "../components/EditarCadastro";
+import HeaderLogin from "../components/HeaderLogin";
+
+const EdiçaoCadastro = () => {
+	return (
+		<div>
+			<HeaderLogin></HeaderLogin>
+			<EditarCadastro></EditarCadastro>
+		</div>
+	)
+}
+
+
+export default EdiçaoCadastro
