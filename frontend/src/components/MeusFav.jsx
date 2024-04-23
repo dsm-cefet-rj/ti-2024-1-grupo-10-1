fd933227592d6ba4b9c78a8651d3f8ed56a96cfc
RodrigoKasama <rodrigo.parracho@gmail.com>
2024-04-23 14:13:05 -0300

    Adaptação dos Favoritos com o back mockado - Incompleto

--- frontend/src/components/MeusFav.jsx
+++ frontend/src/components/MeusFav.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom'; // Importe o Link do react-router-dom
-import useBikeStore from '../store';
+import { Link } from 'react-router-dom';
 import useUserStore from './UserUtils';
-import axios, { AxiosError } from 'axios';
 import { fetchProducts } from './BackendUtils';
+
+// TODO: Prioridade 1: As imagens das bikes não aparecem por algum motivo 
+// TODO: Prioridade 2: Exibir um elemento diferente para quando não houver bikes favoritadas ("Você não possui bikes favoritas por enquanto")
 
 const MeusFav = () => {
 
@@ -11,11 +12,8 @@ const MeusFav = () => {
 
 	const userFavIds = useUserStore((state) => state.user.profile.favs);
 
-	// Deprecated
-	const favoriteBikes = useBikeStore((state) => state.favoriteBikes); // Obtenha as bicicletas favoritadas do store
-
 	// Utiliza o useEffect para executar uma única vez, no carregamento da pagina
-	// Dar fetch no inicio do código para obter todos os produtos, como parametro passamos a função que vai atualizar nosso useState (setProducts)
+	// Fetch ao carregar a pagina para obter todos os produtos, como parametro passamos a função que vai atualizar nosso useState (setProducts)
 	useEffect(() => { fetchProducts(setProducts); }, []);
 
 	/* NÃO APAGAR ESSE COMENTARIO 
@@ -37,21 +35,12 @@ const MeusFav = () => {
 	}, []);
 	*/
 
-	console.log("Ids Favoritados:", userFavIds);
-	console.log("Fetch de produtos:", products);
+	// console.log("Fetch de produtos:", products);
+	// console.log("Ids Favoritados:", userFavIds);
 
 	// Com os ids das bikes favoritadas e a relação de bikes gerar os links/componentes de acordo com os objetos favoritos do usuário
-
-	const userFavoriteBikes = products.filter((product) => { return userFavIds.includes(product.id_prod) });
+	const userFavoriteBikes = products.filter((product) => { return userFavIds.includes(product.id_bike) });
 	console.log("Produtos Favoritados:", userFavoriteBikes);
-
-
-
-	// TODO: Parametrizar os endpoints em um unico arquivo e importá-los quando necessário
-
-
-
-	// console.log(objs_favoritados);
 
 	return (
 		<div className="bg-white">
@@ -61,10 +50,10 @@ const MeusFav = () => {
 				<div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
 					{/* // Mapeie as bicicletas favoritadas pelo cliente */}
 					{userFavoriteBikes.map((bike) => (
-						<Link to={`/bike/${bike.id_prod}`} key={bike.id_prod} className="group relative">
+						<Link to={`/bike/${bike.id_bike}`} key={bike.id_bike} className="group relative">
 							{/* Adicione o Link e especifique um link em BikeSelecionada.jsx */}
 							<div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-								<img src={bike.imageSrc} className="h-full w-full object-cover object-center lg:h-full lg:w-full" />
+								<img src={bike.imagem} className="h-full w-full object-cover object-center lg:h-full lg:w-full" />
 							</div>
 							<div className="mt-4 flex justify-between">
 								<div>
@@ -72,9 +61,9 @@ const MeusFav = () => {
 										<a href={bike.href}>
 											<span aria-hidden="true" className="absolute inset-0" /> {bike.titulo}</a>
 									</h3>
-									<p className="mt-1 text-sm text-gray-500">{bike.color}</p>
+									<p className="mt-1 text-sm text-gray-500">{bike.tipo}</p>
 								</div>
-								<p className="text-sm font-medium text-gray-900">{bike.price}</p>
+								<p className="text-sm font-medium text-gray-900">R$ {bike.valor}</p>
 							</div>
 						</Link>
 					))}
