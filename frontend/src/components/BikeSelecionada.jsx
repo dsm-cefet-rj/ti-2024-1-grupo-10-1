9bd343c45909146a2da9db3dc1e13c23071c1819
RodrigoKasama <rodrigo.parracho@gmail.com>
2024-04-23 20:41:45 -0300

    Ajuste visual - Visualização de Bike

--- frontend/src/components/BikeSelecionada.jsx
+++ frontend/src/components/BikeSelecionada.jsx
@@ -5,33 +5,40 @@ import useUserStore from "./UserUtils";
 import coracao from '../assets/Coraçao.png'
 
 const BikeSelecionada = () => {
+	// Id da bike selecionada
 	const { id } = useParams();
 
+	// Informações da bike a serem carregadas a partir do id
 	const [bike, setBike] = useState({});
+
+	// Informações do vendedor da bike a serem carregadas a partir do id
 	const [vendedor, setVendedor] = useState({});
+
+	// Informação do usuario em relação ao favoritismo da bike
 	const [is_favorite, setFavorite] = useState(false);
 
+	// Coleção de BikesIds do usuário ativo
+	const { userFavs } = useUserStore((state) => ({ userFavs: state.user.profile.favs }));
+
+	// Ao carregar a pagina, será carregado as informações da bike e do vendedor
 	useEffect(() => {
 		fetchProduct(setBike, id);
 		fetchUser(setVendedor, bike.userId);
 	}, []);
 
-	const { userFavs } = useUserStore((state) => ({ userFavs: state.user.profile.favs }));
-
+	// Quando userFavs alterar o valor, atualizaremos a variavel que indicara se a bike é favorita --- Serve para renderizar o texto do botão de favorito 
 	useEffect(() => {
 		setFavorite(userFavs.includes(id));
 	}, [userFavs]);
 
-
-
-
 	if (!bike) return <div> Carregando...</div>;
 
+	// DEBUG --- Erro não consigo verificar a presença ou não da bike nos favoritos.
 	// console.log(bike);
 	// console.log(assoc_user);
-	console.log("Favoritos do Usuario:", userFavs);
-	console.log("BikeId:", id);
-	console.log("Está contido:", userFavs.includes(id));
+	// console.log("Favoritos do Usuario:", userFavs);
+	// console.log("BikeId:", id);
+	// console.log("Está contido:", userFavs.includes(id));
 
 	return (
 		<div className="2xl:container 2xl:mx-auto lg:py-16 lg:px-20 md:py-12 md:px-6 py-9 px-4 ">
@@ -41,7 +48,8 @@ const BikeSelecionada = () => {
 					<p className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 font-normal text-base leading-4 text-gray-600">Home / Bike / {bike.titulo}</p>
 					<h2 className="font-semibold lg:text-4xl text-3xl lg:leading-9 leading-7 text-gray-800 mt-4">{bike.titulo}</h2>
 					<div className=" flex flex-row justify-between  mt-5">
-						<p className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 font-normal text-base leading-4 text-gray-700 hover:underline hover:text-gray-800 duration-100 cursor-pointer">{bike.favCounter} pessoas favoritaram</p>
+						<span className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 font-normal text-base leading-4 text-gray-700 hover:underline hover:text-gray-800 duration-100 cursor-pointer">
+							<img src={coracao} className="w-6 h-6 inline-block mr-1" alt="Coração" />{bike.favCounter} pessoas favoritaram</span>
 					</div>
 					<p className=" font-normal text-base leading-6 text-gray-600 mt-7">{bike.descricao}</p>
 					<p className=" font-semibold lg:text-2xl text-xl lg:leading-6 leading-5 mt-6 ">R$ {bike.valor}</p>
@@ -50,11 +58,11 @@ const BikeSelecionada = () => {
 							<p className=" font-medium text-base leading-4 text-gray-600"><b>Localização/Contato:</b></p>
 						</div>
 						<hr className=" bg-gray-200 w-full my-2" />
-						{/* <div className=" flex flex-row justify-between items-center mt-6">
+						<div className=" flex flex-row justify-between items-center mt-6">
 							<p className="font-medium text-base leading-4 text-gray-600"><b>Descrição:</b></p>
 							{bike.descricao}
 						</div>
-						<hr className=" bg-gray-200 w-full mt-4" /> */}
+						<hr className=" bg-gray-200 w-full mt-4" />
 					</div>
 					<button className="focus:outline-none focus:ring-2 hover:bg-purple-500 focus:ring-offset-2 focus:ring-purple-700 font-medium text-base leading-4 text-white bg-purple-600 w-full py-5 lg:mt-12 mt-6 flex items-center justify-center" >
 						{is_favorite ? "Adicionar aos favoritos" : "Remover dos favoritos"} <img src={coracao} className="w-6 h-6 ml-4" alt="Coração" />
