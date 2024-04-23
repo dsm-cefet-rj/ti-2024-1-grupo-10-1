dbea8b2e5e468fdbbb9a3ef11daca281e4a9a49b
RodrigoKasama <rodrigo.parracho@gmail.com>
2024-04-23 19:50:06 -0300

    Atualização do backendUtils.jsx

--- frontend/src/components/BackendUtils.jsx
+++ frontend/src/components/BackendUtils.jsx
@@ -1,14 +1,35 @@
 import axios from "axios";
 
-
 const PORT = 3000;
 const URL = "http://localhost:" + PORT;
 
 
-export const fetchProducts = async (setTarget) => {
+export const fetchAllProducts = async (setTarget) => {
 	try {
 		const response = await axios.get(URL + "/bike");
 		if (response.status == 200) await setTarget(response.data);
+		else throw AxiosError.ERR_BAD_RESPONSE;
+	} catch (error) {
+		console.error('Ocorreu um erro ao buscar os dados:', error);
+
+	}
+}
+
+export const fetchProduct = async (setTarget, id) => {
+	try {
+		const response = await axios.get(URL + "/bike", { params: { id_bike: id } });
+		if (response.status == 200) await setTarget(response.data[0]);
+		else throw AxiosError.ERR_BAD_RESPONSE;
+	} catch (error) {
+		console.error('Ocorreu um erro ao buscar os dados:', error);
+
+	}
+}
+
+export const fetchUser = async (setTarget, id) => {
+	try {
+		const response = await axios.get(URL + "/users", { params: {id: id } });
+		if (response.status == 200) await setTarget(response.data[0]);
 		else throw AxiosError.ERR_BAD_RESPONSE;
 	} catch (error) {
 		console.error('Ocorreu um erro ao buscar os dados:', error);
