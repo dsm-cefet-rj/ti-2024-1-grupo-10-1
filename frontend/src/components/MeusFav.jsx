e6385a071735b7a4a17fffbff3a0e8997b96c502
RodrigoKasama <rodrigo.parracho@gmail.com>
2024-04-23 11:48:52 -0300

    Utilização de BackendUtils para simplificação da seção de Favoritos

--- frontend/src/components/MeusFav.jsx
+++ frontend/src/components/MeusFav.jsx
@@ -3,16 +3,22 @@ import { Link } from 'react-router-dom'; // Importe o Link do react-router-dom
 import useBikeStore from '../store';
 import useUserStore from './UserUtils';
 import axios, { AxiosError } from 'axios';
-
+import { fetchProducts } from './BackendUtils';
 
 const MeusFav = () => {
 
 	const [products, setProducts] = useState([]);
 
+	const userFavIds = useUserStore((state) => state.user.profile.favs);
+
+	// Deprecated
 	const favoriteBikes = useBikeStore((state) => state.favoriteBikes); // Obtenha as bicicletas favoritadas do store
 
-	const userFavsIds = useUserStore((state) => state.user.profile.favs);
+	// Utiliza o useEffect para executar uma única vez, no carregamento da pagina
+	// Dar fetch no inicio do código para obter todos os produtos, como parametro passamos a função que vai atualizar nosso useState (setProducts)
+	useEffect(() => { fetchProducts(setProducts); }, []);
 
+	/* NÃO APAGAR ESSE COMENTARIO 
 	useEffect(() => {
 
 		const fetchProducts = async () => {
@@ -27,17 +33,20 @@ const MeusFav = () => {
 			}
 		}
 
-		fetchProducts();
-
+		fetchProducts(setProducts);
 	}, []);
+	*/
+
+	console.log("Ids Favoritados:", userFavIds);
+	console.log("Fetch de produtos:", products);
+
+	// Com os ids das bikes favoritadas e a relação de bikes gerar os links/componentes de acordo com os objetos favoritos do usuário
+
+	const userFavoriteBikes = products.filter((product) => { return userFavIds.includes(product.id_prod) });
+	console.log("Produtos Favoritados:", userFavoriteBikes);
 
 
-	console.log("Todos os produtos:", products);
-	console.log("Ids Favoritados:", userFavsIds);
 
-	let a = products.filter((product) => { return userFavsIds.includes(product.id_prod) });
-	console.log("Produtos Favoritados:", a);
-	// Com os ids das bikes favoritadas, extrair do server a relação de bikes e gerar os links/componentes de acordo
 	// TODO: Parametrizar os endpoints em um unico arquivo e importá-los quando necessário
 
 
@@ -51,17 +60,17 @@ const MeusFav = () => {
 
 				<div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
 					{/* // Mapeie as bicicletas favoritadas pelo cliente */}
-					{favoriteBikes.map((bike) => (
-						<Link to={`/bike/${bike.id}`} key={bike.id} className="group relative">
+					{userFavoriteBikes.map((bike) => (
+						<Link to={`/bike/${bike.id_prod}`} key={bike.id_prod} className="group relative">
 							{/* Adicione o Link e especifique um link em BikeSelecionada.jsx */}
 							<div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-								<img src={bike.imageSrc} alt={bike.imageAlt} className="h-full w-full object-cover object-center lg:h-full lg:w-full" />
+								<img src={bike.imageSrc} className="h-full w-full object-cover object-center lg:h-full lg:w-full" />
 							</div>
 							<div className="mt-4 flex justify-between">
 								<div>
 									<h3 className="text-sm text-gray-700">
 										<a href={bike.href}>
-											<span aria-hidden="true" className="absolute inset-0" /> {bike.name}</a>
+											<span aria-hidden="true" className="absolute inset-0" /> {bike.titulo}</a>
 									</h3>
 									<p className="mt-1 text-sm text-gray-500">{bike.color}</p>
 								</div>
