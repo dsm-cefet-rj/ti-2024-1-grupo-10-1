e357ee2fb9cd41f332b738c87bb9881a54904fef
RodrigoKasama <rodrigo.parracho@gmail.com>
2024-08-28 09:52:37 -0300

    Inicio de endpoints usuários

--- backend/backbike/routes/users.route.js
+++ backend/backbike/routes/users.route.js
@@ -2,56 +2,36 @@ var express = require('express');
 var router = express.Router();
 
 var Usuario = require("../models/user.schema")
-let users = [
-	{
-		"id": "1",
-		"nome": "Rodrigo Teixeira Parracho",
-		"CEP": "12341234",
-		"email": "parracho@gmail.com",
-		"senha": "1234",
-		"FavCollection": [
-			1,
-			3,
-			2
-		],
-		"telefone": "21973641659"
-	},
-	{
-		"id": "2",
-		"nome": "Joao Jendiroba",
-		"CEP": "12345678",
-		"email": "jendiroba@gmail.com",
-		"senha": "1234",
-		"FavCollection": [],
-		"telefone": "21973641659"
-	},
-	{
-		"id": "3",
-		"nome": "Gabriel Alves Giuliano",
-		"CEP": "11111111",
-		"email": "gabriel@gmail.com",
-		"senha": "1234",
-		"FavCollection": [],
-		"telefone": "21973641659"
-	}
-]
+
+router.route("/")
+	/* GET users listing. */
+	.get(function (req, res, next) {
+		Usuario.find({}).then((data) => {
+			res.json(data)
+		}).catch((err) => {
+
+		});
+	})
+	// Add user
+	.post((req, res, next) => {
+		const newUser = req.body;
+		Usuario.create(newUser).then((newUser) => {
+			res.json({ objAdded: newUser, "status": "OK" });
+		}).catch((err) => {
+			res.json({ objAdded: newUser, "status": "ERROR", "message": err.message });
+		});
+	});
 
 
-/* GET users listing. */
-router.get('/', function (req, res, next) {
+router.route("/:id")
+	.get((req, res, next) => {
+		input_id = req.params.id
+		res.json(users[input_id])
+		// Usuario.findById(input_id).then((UserData) => {
+		// 	res.json(UserData)
+		// }).catch((error) => { res.status(500).json({ message: error.message }) })
+	})
 
-	// res.statusCode = 200
-	// res.setHeader("Content-Type", "aplication/json")
-	res.json(users)
-});
-
-router.route("/:id").get((req, res, next) => {
-	input_id = req.params.id
-	res.json(users[input_id])
-	// Usuario.findById(input_id).then((UserData) => {
-	// 	res.json(UserData)
-	// }).catch((error) => { res.status(500).json({ message: error.message }) })
-});
 
 
 
