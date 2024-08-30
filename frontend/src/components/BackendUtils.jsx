61cadd45b8205b73204b598802f48455c10d47fd
RodrigoKasama <rodrigo.parracho@gmail.com>
2024-08-30 13:46:08 -0300

    Integração Backend e Frontend - Carregamento das bikes na pagina principal

--- frontend/src/components/BackendUtils.jsx
+++ frontend/src/components/BackendUtils.jsx
@@ -3,16 +3,17 @@
 
 import axios from "axios";
 
-const PORT = 3000;
+const PORT = 3015;
 const URL = "http://localhost:" + PORT;
 
-const user_endpoint = URL + "/users";
-const bike_endpoint = URL + "/bike";
+const user_endpoint ="/users";
+const bike_endpoint = "/bike";
+const feedback_endpoint = "/feedback";
 
 
 export const fetchAllProducts = async (setTarget) => {
 	try {
-		const response = await axios.get(URL + "/bike");
+		const response = await axios.get(URL + bike_endpoint);
 		if (response.status == 200) await setTarget(response.data);
 		else throw AxiosError.ERR_BAD_RESPONSE;
 	} catch (error) {
@@ -23,7 +24,7 @@ export const fetchAllProducts = async (setTarget) => {
 
 export const fetchProduct = async (setTarget, id) => {
 	try {
-		const response = await axios.get(URL + "/bike", { params: { id_bike: id } });
+		const response = await axios.get(URL + bike_endpoint, { params: { id_bike: id } });
 		if (response.status == 200) await setTarget(response.data[0]);
 		else throw AxiosError.ERR_BAD_RESPONSE;
 	} catch (error) {
@@ -34,7 +35,7 @@ export const fetchProduct = async (setTarget, id) => {
 
 export const fetchUser = async (setTarget, userid) => {
 	try {
-		const response = await axios.get(`http://localhost:3000/users/${userid}`);
+		const response = await axios.get(URL + user_endpoint +`${userid}`);
 		setTarget(response.data);
 		
 	} catch (error) {
@@ -56,7 +57,7 @@ export const PostUser = async (newUser) => {
 
 	try {
 		const response = await axios.post(URL + "/users", newUser);
-		// Fazer algo com o response
+		// Verificar se a resposta do backend foi correta
 	} catch (error) {
 		console.error('Erro ao inserir novo usuario a lista de usuarios:', error);
 	}
@@ -65,11 +66,10 @@ export const PostUser = async (newUser) => {
 export const PutUser = async (user_data) => {
 	
 	try {
-		const response = await axios.put(user_endpoint + "/" + user_data.id, user_data);
+		const response = await axios.patch(URL + user_endpoint + "/" + user_data.id, user_data);
 
 	} catch (error) {
-		console.error('Erro aoatualizar os dados do usuario:', error);
-		
+		console.error('Erro ao atualizar os dados do usuario:', error);
 	}
 	
 };
@@ -78,7 +78,7 @@ export const PutUser = async (user_data) => {
 
 export const fetchFeedbacks = async () => {
 	try {
-		const resp = await axios.get(URL);
+		const resp = await axios.get(URL + feedback_endpoint);
 		// if (response.status == 200) await setTarget(response.data);
 	} catch (error) {
 		console.error('Erro ao carregar os feedbacks dos usuários:', error);
--- frontend/src/components/Recomendados.jsx
+++ frontend/src/components/Recomendados.jsx
@@ -7,20 +7,21 @@ import { fetchAllProducts } from './BackendUtils';
 const Recomendados = () => {
 	
 	const [produtos, setProdutos] = useState([]);
-	
 
 	useEffect(() => {
 		fetchAllProducts(setProdutos);
 	}, []);
-
+	/*
+		Modelo:
+	*/
 	return (
 		<div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-4 mt-10 mx-5">
 			{produtos.map((produto) => (
-				<Link key={produto.id_bike} to={`/bike/${produto.id_bike}`} className="relative bg-black">
-					<img src={produto.imagem} className="object-cover w-full h-72" alt={produto.titulo} />
+				<Link key={produto.bikeId} to={`/bike/${produto.bikeId}`} className="relative bg-black">
+					<img src={produto.imagem} className="object-cover w-full h-72" alt={produto.title} />
 					<div className="absolute inset-x-0 bottom-0 bg-black bg-opacity-75 text-white px-4 py-2">
-						<p className="text-lg">Preço: {produto.valor}</p>
-						<p className="text-sm">{produto.descricao}</p>
+						<p className="text-lg">Preço: {produto.price}</p>
+						<p className="text-sm">{produto.description}</p>
 					</div>
 				</Link>
 			))}
