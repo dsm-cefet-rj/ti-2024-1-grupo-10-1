5c276f7b520bbb02a1f76c1ec5a2dc01d1cc1011
RodrigoKasama <rodrigo.parracho@gmail.com>
2024-09-06 12:28:49 -0300

    Consumação da rota de favoritos e integração com o Frontend - Necessita checar o id via estado

--- frontend/src/components/BackendUtils.jsx
+++ frontend/src/components/BackendUtils.jsx
@@ -100,7 +100,19 @@ export const fetchFeedbacks = async () => {
 
 // ?????????
 export const fetchFavorites = async (id) => { 
+	try {
+		// Busca um produto específico (bicicleta) pelo id
+		const response = await axios.get(URL + user_endpoint + "/favorites/" + id); // Envia uma requisição GET para /bike/:id
 
+		if (response.status == 200) {
+			return response.data;
+		} else {
+			throw AxiosError.ERR_BAD_RESPONSE;
+		}
+	} catch (error) {
+		console.error("Ocorreu um erro ao buscar os dados:", error);
+		return [];
+	}
 };
 
 // NÃO MEXA NESTA CARALHA DE FUNÇÃO
@@ -116,7 +128,6 @@ export const fetchLogin = async (email, senha) => {
 		if (response.status === 200) {
 			localStorage.setItem("token", response.data.token); //Usando armazenamento local do browser para fixar o token do usuario logado
 			console.log("Login realizado com sucesso:", response.data);
-			console.log(response.data);
 			// Redirecionar o usuário ou realizar alguma outra ação
 		}
 		return response.data;
--- frontend/src/components/MeusFav.jsx
+++ frontend/src/components/MeusFav.jsx
@@ -1,36 +1,36 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import useUserStore from "./UserUtils";
-import { fetchAllProducts,  } from "./BackendUtils";
-
-import { fetchLogin } from "./BackendUtils";
-
-// ???????
-import { fetchFavorites } from "./BackendUtils";
+import { fetchLogin, fetchFavorites } from "./BackendUtils";
 
 // TODO: Prioridade 1: Exibir um elemento diferente para quando não houver bikes favoritadas ("Você não possui bikes favoritas por enquanto")
 
 const MeusFav = () => {
-	// const [products, setProducts] = useState([]);
+	const [favoritos, setFavoritos] = useState([]);
+
+	// Utiliza o useEffect para executar uma única vez, no carregamento da pagina
+	useEffect(() => {
+		const getFavs = async () => {
+			// Login as parracho -> Será substituido pelo carregamento do estado do usuário
+			const { userId: id } = await fetchLogin("parracho@gmail.com", "12345");
+
+			if (id != null) {
+				// Collect favorites
+				const data_favorites = await fetchFavorites(id);
+				// Check response
+				if (data_favorites != null) {
+					// Set favorite data
+					if (data_favorites.status == "OK") setFavoritos(data_favorites.favs);
+				}
+			}
+		};
+		getFavs();
+	}, []);
 
 	// const userFavIds = useUserStore((state) => state.user.profile.favs);
 	// const logged = useUserStore((state) => state.user.logged);
 
-	let login_as_parracho = async () => {	
-		return await fetchLogin("parracho@gmail.com", "12345");
-	};
-	const { userId: id, ...dados } = login_as_parracho();
-		
-		
-
-
-	// // Utiliza o useEffect para executar uma única vez, no carregamento da pagina
 	// // Fetch ao carregar a pagina para obter todos os produtos, como parametro passamos a função que vai atualizar nosso useState (setProducts)
-	// useEffect(() => { fetchAllProducts(setProducts); }, []);
-
-	// // Com os ids das bikes favoritadas e a relação de bikes gerar os links/componentes de acordo com os objetos favoritos do usuário
-	// const userFavoriteBikes = products.filter((product) => { return userFavIds.includes(product.id_bike) });
-	const userFavoriteBikes = []
 	return (
 		// Se n estiver logado, não mostre nada
 		// logged &&
@@ -40,22 +40,21 @@ const MeusFav = () => {
 
 				<div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
 					{/* // Mapeie as bicicletas favoritadas pelo cliente */}
-					{userFavoriteBikes.map((bike) => (
-						<Link to={`/bike/${bike.id_bike}`} key={bike.id_bike} className="group relative">
-							{/* Adicione o Link e especifique um link em BikeSelecionada.jsx */}
+					{favoritos.map((bike) => (
+						<Link to={`/bike/${bike._id}`} key={bike._id} className="group relative">
+							{/* // Adicione o Link e especifique um link em BikeSelecionada.jsx */}
 							<div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
 								<img src={bike.imagem} className="h-full w-full object-cover object-center lg:h-full lg:w-full" />
 							</div>
 							<div className="mt-4 flex justify-between">
 								<div>
-									<h3 className="text-sm text-gray-700">
-										<a href={bike.href}>
-											<span aria-hidden="true" className="absolute inset-0" /> {bike.titulo}
-										</a>
-									</h3>
 									<p className="mt-1 text-sm text-gray-500">{bike.tipo}</p>
 								</div>
-								<p className="text-sm font-medium text-gray-900">R$ {bike.valor}</p>
+								<p className="text-sm font-medium text-gray-900">R$ {bike.price}</p>
+								{bike.titulo}
+								{/* <h3 className="text-sm text-gray-700">
+									<span aria-hidden="true" className="absolute inset-0"></span>
+								</h3> */}
 							</div>
 						</Link>
 					))}
