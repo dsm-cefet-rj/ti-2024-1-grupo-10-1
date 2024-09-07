220b01d0c53d5d66ab75ac7f6744a0e37a127e6a
RodrigoKasama <rodrigo.parracho@gmail.com>
2024-09-07 12:23:23 -0300

    Acho que consertei a merda que eu fiz..

--- backend/backbike/routes/bikes.route.js
+++ backend/backbike/routes/bikes.route.js
@@ -121,7 +121,7 @@ router.route('/:id')
 
 router.route('/me')
 	// Rota para obter bicicletas postadas pelo usuário logado
-	.get(async (req, res, next) => {
+	.get(authenticate.verifyUser, async (req, res, next) => {
 		try {
 			const userId = req.user._id;
 			const bikes = await Bike.find({ userId }).lean();
