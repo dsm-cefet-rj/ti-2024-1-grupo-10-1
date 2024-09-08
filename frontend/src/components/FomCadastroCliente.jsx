5f368d68a3edfde4aa143daae109fc8bf496d58f
Gabriel-gag <115362448+Gabriel-gag@users.noreply.github.com>
2024-09-07 21:25:24 -0300

    Update FomCadastroCliente.jsx

--- frontend/src/components/FomCadastroCliente.jsx
+++ frontend/src/components/FomCadastroCliente.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { PostUser } from './BackendUtils';
+import axios from 'axios';
 
 export default function FormCliente() {
     const [formData, setFormData] = useState({
