84146d4355027085e6b638c46a0365866cd7fe48
Gabriel-gag <115362448+Gabriel-gag@users.noreply.github.com>
2024-09-07 22:26:17 -0300

    Update MinhaConta.jsx
    Direcionamento para criaçao de feedback

--- frontend/src/components/MinhaConta.jsx
+++ frontend/src/components/MinhaConta.jsx
@@ -70,6 +70,20 @@ const Minhaconta = () => {
 						</div>
 					</a>
 				</div>
+				<div className="border-b">
+					<Link to="/feedback" className="px-4 py-2 hover:bg-gray-100 flex">
+						<div className="text-gray-800">
+							<svg className="w-5 h-5" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" viewBox="0 0 24 24">
+								<path d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
+							</svg>
+						</div>
+
+						<div className="pl-3">
+							<p className="text-sm font-medium text-gray-800 leading-none">Feedback</p>
+							<p className="text-xs text-gray-500">Deixe sua opinião</p>
+						</div>
+					</Link>
+				</div>				
 
 				<div className="">
 					<Link className="w-full px-4 py-2 pb-4 hover:bg-gray-100 flex" onClick={handleLogout} to="/home">
